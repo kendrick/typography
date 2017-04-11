f76bc1b9247f9e080ef4bfeb578f7a04d71a5fc3
Kendrick Arnett <kmarnett@gmail.com>
2017-04-11 11:23:48 -0500

    Remove .gitignored files.

--- gulp-tasks/build.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  'use strict';
-
-  module.exports = function (gulp, plugins, config) {
-    return function (done) {
-      return plugins.runSequence('copy', ['styles', 'views'], 'sitemap', done);
-    };
-  };
-})();
--- gulp-tasks/default.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  'use strict';
-
-  module.exports = function (gulp, plugins, config) {
-    return function (done) {
-      return plugins.runSequence('build', ['server', 'watch'], done);
-    };
-  };
-})();
